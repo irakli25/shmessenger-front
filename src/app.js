893c3e11862bae3cb709fbd28efca8ec8e096b72
chxei <chxei5667@gmail.com>
2021-06-03 21:09:36 +0400

    a

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ async function postData(url = '', data = {}) {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, *cors, same-origin
     headers: {
-      'Content-Type': 'application/json' 
+      'Content-Type': 'application/json'
     },
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
@@ -14,67 +14,17 @@ async function postData(url = '', data = {}) {
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
-// Example GET method implementation:
-async function getData(url = '', jwt) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 
-  });
-  return response; // parses JSON response into native JavaScript objects
-}
-
-// postData('http://localhost:8080/authenticate', {
-//   username:"chxei",
-//   password:"chxei"
-// })
-//   .then(data => {
-//     console.log(data.jwt); // JSON data parsed by data.json() call
-//       getData('http://localhost:8080/users',data.jwt)
-//       .then(data => {
-//         console.log(data);
-//       })
-//       .catch((error) => {
-//       console.log(error);
-//       })
-//   }).catch((error) => {
-//     console.error('Error:', error);
-//   });
-
-getData('http://localhost:8080/country/getAll')
-.then(data => {
-  console.log(data);
-})
-.catch((error) => {
-console.error('Error:', error);
-})
+const myHeaders = new Headers();
+myHeaders.append('Content-Type', 'application/json');
+myHeaders.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjaHhlaSIsImlhdCI6MTYyMjczOTc5NiwiZXhwIjoxNjIyNzc1Nzk2fQ.EYMfAO4oGCFmzVClgJ1r19pSWaKkG2thArWnT_kpWd0')
 
 
+const myRequest = new Request('http://localhost:8080/user/getAll', {
+  method: 'GET',
+  headers: myHeaders,
+  mode: 'cors'
+});
 
-// const content = 'Hello World';
-// const myHeaders = new Headers();
-// myHeaders.append('Content-Type', 'application/json');
-// myHeaders.append('Content-Length', content.length.toString());
-// myHeaders.append('X-Custom-Header', 'ProcessThisImmediately');
-// myHeaders.append('Vary', 'Origin')
-// myHeaders.append('Vary', 'Access-Control-Request-Method')
-// myHeaders.append('Vary', 'Access-Control-Request-Headers')
-// myHeaders.append('Vary', 'Access-Control-Allow-Origin')
-// myHeaders.append('Access-Control-Allow-Origin', 'google.com')
-
-
-// const myRequest = new Request('http://localhost:8080/country/getAll', {
-//   method: 'GET',
-//    headers: myHeaders,
-//   mode: 'cors'
-// });
-
-// fetch(myRequest)
-//   .then(response => console.log(response.json()));
+fetch(myRequest)
+  .then(response => console.log(response.json()));
